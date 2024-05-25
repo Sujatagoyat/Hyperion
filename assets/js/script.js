590ced33e40181c5a7846ef80143d1540369000e
Sujatagoyat <sujatagoyat8@gmail.com>
2024-05-25 15:39:47 +0530

    check-page

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,15 @@ function opeNav(){
     document.body.classList.toggle("overFlowHidden")
     document.querySelector(".menuIcon").classList.toggle("cross")
   }
+  const topBtn = document.querySelector(".topBtn");
+  const show = document.querySelector(".show"); 
+      window.addEventListener("scroll", function () {
+        if (window.scrollY >600) {
+            topBtn.classList.add("show");
+        } else {
+            topBtn.classList.remove("show");
+        } 
+    })
+    topBtn.addEventListener("click", function () {
+        document.documentElement.scrollTop = 0;
+    })
